801b2f407fc6b7d8b74482d682aec9759893abdc
Shubh <shubhjhawar78@gmail.com>
2024-01-31 00:43:11 -0500

    commit

--- src/App.test.js
+++ src/App.test.js
@@ -1,6 +1,45 @@
-import { render, screen } from "@testing-library/react";
+import { render, screen, waitForElementToBeRemoved} from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+import mockFetch from "./mocks/mockFetch";
 import App from "./App";
 
-test("renders the landing page", () => {
+beforeEach(() =>{
+  jest.spyOn(window, "fetch").mockImplementation(mockFetch);
+})
+
+test("renders the landing page", async () => {
   render(<App />);
+
+  expect(screen.getByRole("heading")).toHaveTextContent(/Doggy Directory/);
+  expect(screen.getByRole("combobox")).toHaveDisplayValue("Select a breed");
+  expect(await screen.findByRole("option", {name:"husky"})).toBeInTheDocument();
+  expect(screen.getByRole("button", {name: "Search"})).toBeDisabled();
+  expect(screen.getByRole("img")).toBeInTheDocument();
 });
+
+
+test("should be able to search and display dog image results", async () => {
+  render(<App />);
+
+  const select = screen.getByRole("combobox");
+  expect(await screen.findByRole("option", {name : 'cattledog'})).toBeInTheDocument();
+  userEvent.selectOptions(select, "cattledog");
+  expect(select).toHaveValue("cattledog");
+
+  const searchBtn = screen.getByRole("button", {name : "Search"})
+  expect(searchBtn).not.toBeDisabled();
+  userEvent.click(searchBtn);
+
+  await waitForElementToBeRemoved(() => screen.queryByText(/Loading/i));
+
+  const dogImages = screen.getAllByRole("img");
+  expect(dogImages).toHaveLength(2);
+  expect(screen.getByText(/2 Results/i)).toBeInTheDocument();
+  expect(dogImages[0]).toHaveAccessibleName("cattledog 1 of 2")
+  expect(dogImages[1]).toHaveAccessibleName("cattledog 2 of 2")
+})
+
+
+afterEach(() => {
+  jest.restoreAllMocks()
+})
